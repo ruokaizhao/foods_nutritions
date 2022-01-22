a64c57ea7811e6c40064f266f882d36a3e35ed96
Ruokai Zhao <lelouch_l@icloud.com>
2022-01-21 22:47:34 -0500

    Adds a state to decide sorting ascending or descending

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,18 +1,29 @@
 import React, { useState } from "react";
 
 function Sort({ onFoods, onSetFoods }) {
+  const [isAscending, setIsAscending] = useState(false)
   const [sortValue, setSortValue] = useState("energy")
 
   function handleChange(e) {
     setSortValue(e.target.value)
   }
 
-  function handleSort() {
+
+  function handleSortAscending() {
+    const sortedFoods = [...onFoods].sort(function(a, b) {
+      return a["nutrition-per-100g"][sortValue] - b["nutrition-per-100g"][sortValue]
+    })
+    onSetFoods(sortedFoods) 
+  }
+
+  function handleSortDescending() {
     const sortedFoods = [...onFoods].sort(function(a, b) {
       return b["nutrition-per-100g"][sortValue] - a["nutrition-per-100g"][sortValue]
     })
     onSetFoods(sortedFoods) 
   }
+
+  
   
 
 
@@ -25,7 +36,9 @@ function Sort({ onFoods, onSetFoods }) {
         <option value="fat">Fat</option>
         <option value="carbohydrate">Carbohydrate</option>
       </select>
-      <button onClick={handleSort}>Sort</button>
+      <button onClick={isAscending ? handleSortAscending : handleSortDescending}>
+        {isAscending ? "Sort Ascending" : "Sort Descending"}
+      </button>
     </div>   
   )
 }
