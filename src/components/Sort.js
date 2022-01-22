5ae062615a0a074917155b56256c253b6a254350
Ruokai Zhao <lelouch_l@icloud.com>
2022-01-21 22:20:11 -0500

    Add select to Sort.js to enable user to sort based on different properties

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,5 +1,17 @@
 import React from "react";
 
-function Sort() {
-    
+function Sort({ onFoods, onSetFoods }) {
+  const sortedFoods = onFoods.sort
+
+
+
+  return (
+    <select>
+      <option value="energy">Energy</option>
+      <option value="protein">Protein</option>
+      <option value="fat">Fat</option>
+      <option value="carbohydrate">Carbohydrate</option>
+    </select>
+  )
+
 }
