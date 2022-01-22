692108ce67525ae3a90d298302b0cedbb862b52c
Ruokai Zhao <lelouch_l@icloud.com>
2022-01-21 21:05:13 -0500

    Add form tag to handle user input

--- src/components/NewFood.js
+++ src/components/NewFood.js
@@ -13,4 +13,55 @@ function NewFood({ onFoods, onSetFoods }) {
   })
 
 
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text" 
+        placeholder="Enter food name" 
+        name="name" 
+        value={submit.name}
+        onChange={handleChange}
+        />
+      <input 
+        type="text" 
+        placeholder="Enter food photo url" 
+        name="url" 
+        value={submit.url}
+        onChange={handleChange}
+        />
+      <input 
+        type="text"
+        placeholder="Enter food energy per 100g"
+        name="energy"
+        value={submit["nutrition-per-100g"].energy}
+        onChange={handleNestedChange}
+        />
+      <input 
+        type="text" 
+        placeholder="Enter food protein per 100g"
+        name="protein"
+        value={submit["nutrition-per-100g"].protein}
+        onChange={handleNestedChange}
+        />
+      <input 
+        type="text" 
+        placeholder="Enter food fat per 100g"
+        name="fat"
+        value={submit["nutrition-per-100g"].fat}
+        onChange={handleNestedChange}
+        />
+      <input 
+        type="text" 
+        placeholder="Enter food carbs per 100g"
+        name="carbohydrate"
+        value={submit["nutrition-per-100g"].carbohydrate}
+        onChange={handleNestedChange}
+        />
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
 export default NewFood
